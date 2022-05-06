a1e81f5f9e44730e0fc35914773d2263a74b738e
mcyos118 <mcgreencode118@gmail.com>
2022-05-06 23:59:32 +0900

    init

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, usdtAddress: string, lpAddress:string} = {
+  cakeAddress: '0xe35FBF0A5EdDA08741A97446163Dd0cFf4B9cDeD',
+  usdtAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+}
+
+export default priceContracts
